f8dac36d7b69cb72c9881a042cb5a9a752504ac6
Brian Spence <brianmspence@gmail.com>
2019-11-07 21:26:51 -0800

    Update Formatting and Unit Tests
    Updated prettier formatting config and formatted more files.
    Added more tests
    Fixed bug where '1d0' was returning 1
    Fixed Typescript error with jest not understanding scope as optional

--- src/__tests__/DiceEvaluator.test.ts
+++ src/__tests__/DiceEvaluator.test.ts
@@ -1,6 +1,75 @@
-import { DiceEvaluator } from '../index';
+import { DiceEvaluator } from '..'
 
-test('Dice Evaluatore', () => {
-  const evaluator = new DiceEvaluator();
-  expect(evaluator.eval('1+1')).toBe(2);
-});
+declare global {
+  namespace jest {
+    // tslint:disable-next-line
+    interface Matchers<R, T> {
+      toBeWithinRange(a: number, b: number): R
+    }
+  }
+}
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      }
+    } else {
+      return {
+        message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      }
+    }
+  },
+})
+
+test('Simple Math', () => {
+  const evaluator = new DiceEvaluator()
+  expect(evaluator.eval('1+1')).toBe(2)
+  expect(evaluator.eval('1-1')).toBe(0)
+  expect(evaluator.eval('1*2')).toBe(2)
+  expect(evaluator.eval('4/2')).toBe(2)
+})
+
+test('Simple Rolls', () => {
+  const evaluator = new DiceEvaluator()
+  expect(evaluator.eval('0d0')).toBe(0)
+  expect(evaluator.eval('1d0')).toBe(0)
+  expect(evaluator.eval('0d1')).toBe(0)
+  expect(evaluator.eval('1d1')).toBe(1)
+  expect(evaluator.eval('2d1')).toBe(2)
+  expect(evaluator.eval('1d4')).toBeWithinRange(1, 4)
+  expect(evaluator.eval('1d6')).toBeWithinRange(1, 6)
+  expect(evaluator.eval('1d8')).toBeWithinRange(1, 8)
+  expect(evaluator.eval('1d10')).toBeWithinRange(1, 10)
+  expect(evaluator.eval('1d12')).toBeWithinRange(1, 12)
+  expect(evaluator.eval('1d20')).toBeWithinRange(1, 20)
+  expect(evaluator.eval('1d100')).toBeWithinRange(1, 100)
+  expect(evaluator.eval('2d6')).toBeWithinRange(2, 12)
+  expect(evaluator.eval('3d6')).toBeWithinRange(3, 18)
+})
+
+test('Roll with math', () => {
+  const evaluator = new DiceEvaluator()
+  expect(evaluator.eval('1d1+1')).toBe(2)
+  expect(evaluator.eval('2d6*0')).toBe(0)
+  expect(evaluator.eval('1d20+5')).toBeWithinRange(6, 25)
+})
+
+test('Advanced Roll Functions', () => {
+  const evaluator = new DiceEvaluator()
+  expect(evaluator.eval('adv(1d20)')).toBeWithinRange(1, 20)
+  expect(evaluator.eval('adv(1d20+5)')).toBeWithinRange(6, 25)
+  expect(evaluator.eval('adv(1d20)+5')).toBeWithinRange(6, 25)
+})
+
+test('Roll constants', () => {
+  const evaluator = new DiceEvaluator({ int: 2 })
+  expect(evaluator.eval('int+1')).toBe(3)
+  expect(evaluator.eval('1d20+int')).toBeWithinRange(3, 23)
+  expect(evaluator.eval('int+prof', { prof: 3 })).toBe(5)
+  expect(evaluator.eval('1d20+int+prof', { prof: 3 })).toBeWithinRange(6, 26)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,43 @@
-import { all, create, randomInt } from 'mathjs';
+import { all, create, MathJsStatic, randomInt } from 'mathjs'
 
 function roll(num: number, sides: number) {
-  const rolls = [];
-  for (let i = 0; i < num; i++) {
-    const r = randomInt(1, sides + 1);
-    rolls.push(r);
+  if (num < 1 || sides < 1) {
+    return 0
   }
-  return rolls.reduce((a, b) => a + b, 0);
+
+  const rolls = []
+  for (let i = 0; i < num; i++) {
+    const r = randomInt(1, sides + 1)
+    rolls.push(r)
+  }
+  return rolls.reduce((a, b) => a + b, 0)
 }
 
 function adv(args: any, math: any, scope: any) {
-  const a = args[0].compile().evaluate(scope);
-  const b = args[0].compile().evaluate(scope);
-  return math.max(a, b);
+  const a = args[0].compile().evaluate(scope)
+  const b = args[0].compile().evaluate(scope)
+  return math.max(a, b)
 }
 
-adv.rawArgs = true;
-
-// const tests = [
-//   '1d20+5',
-//   '1d100',
-//   '1d8+2d4+5',
-//   '6+6',
-//   '4d9*2',
-//   '4d4+int+prof',
-//   'adv(1d20+5)',
-//   'adv(1d20)-2'
-//   ]
-
-// tests.forEach(t =>{
-//   console.log(t)
-//   console.log('result: '+ calc(t))
-//   console.log('---')
-// })
+adv.rawArgs = true
 
 export class DiceEvaluator {
-  private math: any;
+  private math: MathJsStatic
 
-  constructor() {
-    this.math = create(all, {});
-    this.math.import({
-      adv,
-      int: 5,
-      prof: 3,
-      roll,
-    });
+  constructor(scope?: object) {
+    this.math = create(all, {}) as MathJsStatic
+    this.math.import(
+      {
+        ...scope,
+        adv,
+        roll,
+      },
+      {},
+    )
   }
 
-  public eval(exp: string) {
-    const eq = exp.replace(/(\d+)d(\d+)/g, 'roll($1,$2)');
-    return this.math.evaluate(eq);
+  public eval(exp: string, scope: object = {}) {
+    const eq = exp.replace(/(\d+)d(\d+)/g, 'roll($1,$2)')
+    return this.math.evaluate(eq, scope)
   }
 }
