25b0dba06628330622431a2315ca473704325578
Brian Spence <brianmspence@gmail.com>
2019-12-02 00:39:41 +0000

    Add max, min, and dice rolls mets info
    Add qe function that returns result only
    Update eval function to return object with result,
    max possible, min possible, and numbers rolled
    
    Update existing test to use new qe function
    Add tests for min, max, and dice tolls

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
 import { all, create, MathJsStatic, randomInt } from 'mathjs'
-
-function roll(num: number, sides: number) {
-  if (num < 1 || sides < 1) {
-    return 0
-  }
-
-  const rolls = []
-  for (let i = 0; i < num; i++) {
-    const r = randomInt(1, sides + 1)
-    rolls.push(r)
-  }
-  return rolls.reduce((a, b) => a + b, 0)
-}
-
 function adv(args: any, math: any, scope: any) {
   const a = args[0].compile().evaluate(scope)
   const b = args[0].compile().evaluate(scope)
@@ -23,21 +9,58 @@ adv.rawArgs = true
 
 export class DiceEvaluator {
   private math: MathJsStatic
+  private dice: number[]
 
   constructor(scope?: object) {
+    this.dice = []
     this.math = create(all, {}) as MathJsStatic
     this.math.import(
       {
         ...scope,
         adv,
-        roll,
+        roll: this.roll,
       },
       {},
     )
   }
 
   public eval(exp: string, scope: object = {}) {
+    this.dice = []
     const eq = exp.replace(/(\d+)d(\d+)/g, 'roll($1,$2)')
-    return this.math.evaluate(eq, scope)
+    const result = this.math.evaluate(eq, scope)
+
+    const minEq = exp.replace(/(\d+)d(\d+)/g, '$1')
+    const min = this.math.evaluate(minEq, scope)
+
+    const maxEq = exp.replace(/(\d+)d(\d+)/g, '\($1*$2\)')
+    const max = this.math.evaluate(maxEq, scope)
+
+    const dice: number[] = this.dice.slice()
+
+    return {
+      dice,
+      max,
+      min,
+      result,
+    }
+  }
+
+  public qe(exp: string, scope: object = {}) {
+    return this.eval(exp, scope).result
+  }
+
+  private roll = (num: number, sides: number) => {
+    if (num < 1 || sides < 1) {
+      return 0
+    }
+
+    const rolls = []
+    for (let i = 0; i < num; i++) {
+      const r = randomInt(1, sides + 1)
+      this.dice.push(r)
+      rolls.push(r)
+    }
+
+    return rolls.reduce((a, b) => a + b, 0)
   }
 }
